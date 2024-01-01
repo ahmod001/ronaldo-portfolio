04adc3d23b4fb826607270dc238a8da3cdd5c1ad
ahmod001 <ahmodh08@gmail.com>
2024-01-01 10:58:35 +0600

    assets imported

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -6,12 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ronaldo Fredrickson</title>
+
+
+    <link href={{ asset('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900') }}
+        rel="stylesheet">
+
+    <link rel="stylesheet" href={{ asset('/css/open-iconic-bootstrap.min.css') }}>
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href={{ asset('/css/owl.carousel.min.css') }}>
+    <link rel="stylesheet" href={{ asset('/css/owl.theme.default.min.css') }}>
+    <link rel="stylesheet" href={{ asset('/css/magnific-popup.css') }}>
+
+    <link rel="stylesheet" href={{ asset('/css/aos.css') }}>
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href={{ asset('/css/flaticon.css') }}>
+    <link rel="stylesheet" href={{ asset('/css/icomoon.css') }}>
+    <link rel="stylesheet" href={{ asset('/css/style.css') }}>
 </head>
 
-<body>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4"
+                stroke="#eeeeee" />
+            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4"
+                stroke-miterlimit="10" stroke="#F96D00" />
+        </svg>
+    </div>
+
+
     <main>
         @yield('content')
     </main>
+
+    <script src={{ asset('/js/jquery.min.js') }}></script>
+    <script src={{ asset('/js/jquery-migrate-3.0.1.min.js') }}></script>
+    <script src={{ asset('/js/popper.min.js') }}></script>
+    <script src={{ asset('/js/bootstrap.min.js') }}></script>
+    <script src={{ asset('/js/jquery.easing.1.3.js') }}></script>
+    <script src={{ asset('/js/jquery.waypoints.min.js') }}></script>
+    <script src={{ asset('/js/jquery.stellar.min.js') }}></script>
+    <script src={{ asset('/js/owl.carousel.min.js') }}></script>
+    <script src={{ asset('/js/jquery.magnific-popup.min.js') }}></script>
+    <script src={{ asset('/js/aos.js') }}></script>
+    <script src={{ asset('/js/jquery.animateNumber.min.js') }}></script>
+    <script src={{ asset('/js/scrollax.min.js') }}></script>
+    <script src={{ asset('/js/main.js') }}></script>
 </body>
 
 </html>
